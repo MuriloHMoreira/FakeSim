23cc40814b5a86a39deae79009c40549c282a146
Murilo Henrique Moreira <moreira.murilo@gmail.com>
2024-06-03 16:55:35 -0300

    Corrigir Propriedades

--- propriedades_5CAC.py
+++ propriedades_5CAC.py
@@ -1,6 +1,20 @@
-def K(P_v, T):
-    A = 0.7341
-    B = 2.7
-    C = 100
-    K_val = A*T**2 + B*P_v/10 + C
-    return K_val
+# def K(P_v, T):
+#     A = 0.7341
+#     B = 2.7
+#     C = 100
+#     K_val = A*T**2 + B*P_v/10 + C
+#     return K_val
+
+def rho(T):
+    rho_val = 2700
+    return rho_val
+
+
+def Cp(T):
+    Cp_val = 700
+    return Cp_val
+
+
+def lmbda(T):
+    lmbda_val = 30
+    return lmbda_val
